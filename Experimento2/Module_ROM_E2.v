f36cfecb734989de626960a3fec66fc565aebd46
liewhnic <isaac.gomez@ucr.ac.cr>
2017-04-18 14:08:09 -0600

    Se cambia el valor de un registro.
    Se cambia el valor de uno de los registros para hacer que la multiplicación sea correcta.

--- Experimento2/Module_ROM_E2.v
+++ Experimento2/Module_ROM_E2.v
@@ -15,7 +15,7 @@ begin
 //LOOP1:
 	0: oInstruction = { `NOP ,24'd4000    };
 	1: oInstruction = { `STO, `R0,16'd0003 };
-	2: oInstruction = { `STO, `R1,16'd0022 };
+		2: oInstruction = { `STO, `R1,16'd0015 };
 	6: oInstruction = { `SUB ,`R0,`R0,`R1    };
 	4: oInstruction = { `NOP ,24'd4000    };
 	5: oInstruction = { `LED ,8'b0,`R0,8'b0 };
