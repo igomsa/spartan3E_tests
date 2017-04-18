cf249a10aa2b2753d6fa581ce7689032fc399176
igsem <ignacio.gamboa@ucr.ac.cr>
2017-04-18 16:19:53 -0600

    For Smul
    Implementacion de for para multiplicacion

--- Experimento2/MiniAlu_E3.v
+++ Experimento2/MiniAlu_E3.v
@@ -18,7 +18,7 @@ wire [3:0]  wOperation;
 reg [15:0]   rResult;
 wire [7:0]  wSourceAddr0,wSourceAddr1,wDestination;
 wire [15:0] wSourceData0,wSourceData1,wIPInitialValue,wImmediateValue;
-wire [10:0]   wCarry;
+wire[15:0] wCarry[14:0];
 wire [5:0] wResult;
 output reg [15:0] rVars;
 
@@ -134,40 +134,59 @@ begin
 	end
         //-------------------------------------
 
-	`MUL:
+	`SMUL:
 	begin
 		rFFLedEN     <= 1'b0;
 		rBranchTaken <= 1'b0;
 		rWriteEnable <= 1'b1;
-		rResult[0]   <= wSourceData1[0] & wSourceData0[0];
-                rVars[0] <= wSourceData1[0] & wSourceData0[1];
-                rVars[1] <= wsourcedata1[1] & wsourcedata0[0];
-                rVars[2] <= wSourceData1[2] & wSourceData0[0];
-                rVars[3] <= wSourceData1[1] & wSourceData0[1];
-                rVars[4] <= wSourceData1[3] & wSourceData0[0];
-                rVars[5] <= wSourceData1[2] & wSourceData0[1];
-                rVars[6] <= 1'b0;
-                rVars[7] <= wSourceData1[3] & wSourceData0[1];
-                rVars[8] <= wSourceData1[0] & wSourceData0[2];
-                rVars[9] <= wSourceData1[1] & wSourceData0[2];
-                rVars[10] <= wSourceData1[2] & wSourceData0[2];
-                rVars[11] <= wSourceData1[3] & wSourceData0[2];
-                rVars[12] <= wSourceData1[0] & wSourceData0[3];
-                rVars[13] <= wSourceData1[1] & wSourceData0[3];
-                rVars[14] <= wSourceData1[2] & wSourceData0[3];
-                rVars[15] <= wSourceData1[3] & wSourceData0[3];
-                EMUL mulr(.wA(rVars[0]), .wB(rVars[1]), .iCarry(rVars[6]), .Clock(Clock), .oCarry(wCarry[0]), .rR(rResult[1]));
-                EMUL mul2(.wA(rVars[2]), .wB(rVars[3]), .iCarry(wCarry[0]), .Clock(Clock), .oCarry(wCarry[1]), .rR(wResutl[0]));
-                EMUL mul3(.wA(rVars[4]), .wB(rVars[5]), .iCarry(wCarry[1]), .Clock(Clock), .oCarry(wCarry[2]), .rR(wResult[1]));
-                EMUL mul4(.wA(rVars[6]), .wB(rVars[7]), .iCarry(wCarry[2]), .Clock(Clock), .oCarry(wCarry[3]), .rR(wResult[2]));
-                EMUL mul5(.wA(wResult[0]), .wB(rVars[8]), .iCarry(1'b0), .Clock(Clock), .oCarry(wCarry[4]), .rR(rResult[2]));
-                EMUL mul6(.wA(wResult[1]), .wB(rVars[9]), .iCarry(wCarry[4]), .Clock(Clock), .oCarry(wCarry[5]), .rR(wResult[3]));
-                EMUL mul7(.wA(wResult[2]), .wB(rVars[10]), .iCarry(wCarry[5]), .Clock(Clock), .oCarry(wCarry[6]), .rR(wResult[4]));
-                EMUL mul8(.wA(rVars[11]), .wB(wCarry[3]), .iCarry(wCarry[6]), .Clock(Clock), .oCarry(wCarry[7]), .rR(wResult[5]));
-                EMUL mul9(.wA(wResult[3]), .wB(rVars[12]), .iCarry(1'b0), .Clock(Clock), .oCarry(wCarry[8]), .rR(rResult[3]));
-                EMUL mul10(.wA(wResult[4]), .wB(rVars[13]), .iCarry(wCarry[8]), .Clock(Clock), .oCarry(wCarry[9]), .rR(rResult[4]));
-                EMUL mul11(.wA(wResult[5]), .wB(rVars[14]), .iCarry(wCarry[9]), .Clock(Clock), .oCarry(wCarry10), .rR(rResult[5]));
-                EMUL mul12(.wA(wCarry[7]), .wB(rVars[15]), .iCarry(wCarry[10]), .Clock(Clock), .oCarry(rResult[7]), .rR(rResult[6]));
+		genvar CurrentRow, CurrentCol;
+		generate
+	for (CurrentRow = 0; CurrentRow < 14; CurrentRow = CurrentRow +1)
+       	begin
+		for ( CurrentCol = 0; CurrentCol < 15; CurrentCol = CurrentCol + 1)
+         	begin 
+          	  if (CurrentCol == 0)
+              {	
+              	assign wCarry[ CurrentRow ][ 0 ] = 0;
+              }
+              else {
+                  if (CurrentRow == 0){
+                    MODULE_ADDER #(4) MyAdder
+                    (
+      		   .A( wSourceData0[CurrentRow] & wSourceData1[CurrentCol] ),
+		   .B( wSourceData0[CurrentRow] & wSourceData1[CurrentCol] ),
+                   .Ci( wCarry[ CurrentRow ][ CurrentCol ] ),
+                   .Co( wCarry[ CurrentRow ][ CurrentCol + 1]),
+                   .Ro(wResult[CurrentCol][CurrentRow])
+                    );                    
+                  }
+			  else if (CurrentCol == 13){
+                  	MODULE_ADDER # (4) MyAdder
+                    (
+		    .A( wSourceData0[CurrentRow] & wSourceData1[CurrentCol] ),
+		    .B( wSourceData0[CurrentRow] & wSourceData1[CurrentCol] ),
+                    .Ci( wCarry[ CurrentRow ][ CurrentCol ] ),
+		    .Co( wCarry[ CurrentRow +1 ][ CurrentCol]),
+                    .Ro(wResult[CurrentCol][CurrentRow])
+                    );
+                  else{
+			  if (CurrentCol != 13){
+                 	 MODULE_ADDER # (4) MyAdder
+                    	(
+                   	.A( wSourceData0[CurrentRow] & wSourceData1[CurrentCol] ),
+		 	.B( wSourceData0[CurrentRow] & wSourceData1[CurrentCol] ),
+                   	.Ci( wCarry[ CurrentRow ][ CurrentCol ] ),
+                   	.Co( wCarry[ CurrentRow ][ CurrentCol + 1]),
+                   	.Ro(wResult[CurrentCol][CurrentRow])
+                    }
+                   
+              }
+          end
+       end
+	endgenerate
+        
+end
+		
 	end
 	//-------------------------------------
 	`STO:
