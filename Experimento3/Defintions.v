47d88a44a65e5ddb87446f129cf637fb7d39ad57
moi6ba <moi6ba@hotmail.com>
2017-05-03 08:58:35 -0600

    Update Defintions.v

--- Experimento3/Defintions.v
+++ Experimento3/Defintions.v
@@ -12,14 +12,21 @@
 `define JMP   4'd6
 `define MUL   4'd7
 
-`define R0 8'd0
-`define R1 8'd1
-`define R2 8'd2
-`define R3 8'd3
-`define R4 8'd4
-`define R5 8'd5
-`define R6 8'd6
-`define R7 8'd7
+//Se agregan las siguientes lineas para implementar las funciones LCD y SHL. Ademas se define la instruccion LCD_INIT para automatizar la inicializacion del LCD.
+`define LCD   4'd8
+`define SHL   4'd9
+`define LCD_INIT	4'd10
+
+//Se agregan las siguientes lineas para poder agregar las letras respectivas a la frase "Hola Mundo". Se definen las letras con la respectiva representacion ASCII.
+`define H	16'b01001000
+`define o	16'b01101111
+`define l	16'b01101100
+`define a	16'b00001010
+`define M	16'b01001101
+`define u	16'b01110101
+`define n	16'b01101110
+`define d	16'b01100100
+
 
 
 `endif
