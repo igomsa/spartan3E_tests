4d423abf1379ccbdc286056223d5d540eb632620
moi6ba <moi6ba@hotmail.com>
2017-05-24 11:29:40 -0600

    Añadida etapa de inicialización

--- Experimento3/Module_ROM_E3.v
+++ Experimento3/Module_ROM_E3.v
@@ -2,7 +2,7 @@
 `include "Defintions.v"
 
 `define LOOP1 8'd8
-`define SUBROUTINE 8'd24
+`define SUBROUTINE 8'd50
 
 module ROM
 (
@@ -15,20 +15,56 @@ begin
 
 
 //LOOP1:
-	0: oInstruction = { `NOP , 24'd4000    	};
-	1: oInstruction = { `STO ,`R0, `M};
-	2: oInstruction = { `CALL, `SUBROUTINE, `iAddress};
-	3: oInstruction = { `STO ,`R0, `a};
-	4: oInstruction = { `CALL, `SUBROUTINE, `iAddress};
+	0: oInstruction = {`LCD_INIT, 24'd0};
+//LCD_INIT:
+	1: oInstruction = {`NOP, 24'd750000};//espera de 15ms
+	2: oInstruction = {`STO, `R0, 16'h3};
+	3: oInstruction = {`SHL, `R0, `R0, 8'd4};
+	4: oInstruction = {`LCD, 8'b0, `R0, 8'b0};
+	5: oInstruction = {`NOP, 24'd205000 };//espera de 4.1ms
+	6: oInstruction = {`LCD, 8'b0, `R0, 8'b0};
+	7: oInstruction = {`NOP, 24'd5000 };//espera de 100us
+	8: oInstruction = {`LCD, 8'b0, `R0, 8'b0};
+	9: oInstruction = {`NOP, 24'd2000};//espera de 40us
+	10: oInstruction = {`STO, `R0, 16'h2};
+	11: oInstruction = {`SHL, `R0, `R0, 8`b4};
+	12: oInstruction = {`LCD, 8'b0, `R0,8'b0};
+	13: oInstruction = {`NOP, 24'd2000};//espera de 40us
+	//termina la inicializacion sigue display clear
+	14: oInstruction = {`STO, `R0, 16'h28};
+	15: oInstruction = {`LCD, 8'b0, `R0,8'b0};
+	16: oInstruction = {`NOP, 24'd50};//espera 1us
+	17: oInstruction = {`SHL, `R0, `R0, 16'd4 };
+	18: oInstruction = {`LCD, 8'b0, `R0, 8'b0};
+	19: oInstruction = {`NOP, 24'd2000};//espera 40us
+	20: oInstruction = {`STO, `R0, 16'h06};
+	21: oInstruction = {`LCD, 8'b0 , `R0, 8'b0};
+	22: oInstruction = {`NOP, 24'd50};//espera 1us
+	23: oInstruction = {`SHL, `R0, `R0, 8'd4};
+	24: oInstruction = {`LCD, 8'b0 , `R0, 8'b0};
+	25: oInstruction = {`NOP, 24'd2000};//espera 40us
+	26: oInstruction = {`STO, `R0, 16'h0C};
+	27: oInstruction = {`LCD, 8'b0 , `R0, 8'b0};
+	28: oInstruction = {`NOP, 24'd50};//espero 1us
+	29: oInstruction = {`SHL, `R0, `R0, 8'd4};
+	30: oInstruction = {`LCD, 8'b0 , `R0, 8'b0};
+	31: oInstruction = {`NOP, 24'd2000};//espera 40us
+	32: oInstruction = {`STO, `R0, 16'h01};
+	33: oInstruction = {`LCD, 8'b0 , `R0, 8'b0};
+	34: oInstruction = {`NOP, 24'd50};//espero 1us
+	35: oInstruction = {`SHL, `R0, `R0, 8'd4};
+	36: oInstruction = {`LCD, 8'b0 , `R0, 8'b0};
+	37: oInstruction = {`NOP, 24'd82000};//espero 4.64ms
+ 	38: oInstruction = { `STO, `R1, `M}; //Carga la letra M
+	39: oInstruction = { `CALL, `SUBROUTINE, iAddress}; //Llama la subrutina
+	40: oInstruction = { `JMP,  8'd0, 16'b0};
 
-
-SUBROUTINE: oInstruction = {`LDC_INIT, 24'd0};
-	25: oInstruction = {`LCD, 8'b0, `R0, 8'b0};
-	26: oInstruction = {`NOP, 24`b1005};
-	27: oInstruction = {`SHL, `R0, `R0, 8'd4};
-	28: oInstruction = {`LCD, 8'b0, `R0, 8'b0};
-	29: oInstruction = {`NOP, 24`b1005};
-	30: oInstruction = {`RET, 24`b00};
+SUBROUTINE: oInstruction = {`LCD, 8'b0, `R1, 8'b0};
+	51: oInstruction = {`NOP, 24'd1005};
+	52: oInstruction = {`SHL, `R1, `R1, 8'd4};
+	53: oInstruction = {`LCD, 8'b0, `R1, 8'b0};
+	54: oInstruction = {`NOP, 24'd1005};
+	55: oInstruction = {`RET, 24'b00};
 
 	default:
 		oInstruction = { `LED ,  24'b10101010 };		//NOP
