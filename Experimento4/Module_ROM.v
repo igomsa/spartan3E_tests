cfb5c12fac563bf66da4634895878cc12178a2f9
moi6ba <moi6ba@hotmail.com>
2017-05-31 12:02:01 -0600

    Update Module_ROM.v

--- Experimento4/Module_ROM.v
+++ Experimento4/Module_ROM.v
@@ -13,13 +13,28 @@ begin
 	case (iAddress)
 
 //LOOP1:
-	0: oInstruction = { `NOP ,24'd4000    };
-	1: oInstruction = { `STO, `R0,16'd0003 };
-	2: oInstruction = { `STO, `R1,-16'd0022 };
-	6: oInstruction = { `MUL ,`R0,`R0,`R1    };
-	4: oInstruction = { `NOP ,24'd4000    };
-	5: oInstruction = { `LED ,8'b0,`R0,8'b0 };
-	6: oInstruction = { `JMP ,  8'd0,16'b0   };
+	0: oInstruction = { `NOP, 24'd4000  };
+	1: oInstruction = { `STO, `R4, 16'd240 };
+	2: oInstruction = { `STO, `R1, 16'd240 };
+	6: oInstruction = { `VGA, `R4,`R1   };
+	7: oInstruction = { `NOP, 24'd4000  };
+		// Incompleto
+/*	8: oInstruction = { `STO, `R4, 16'd241 };
+	9: oInstruction = { `STO, `R1, 16'd241 };
+	10: oInstruction = { `VGA, `R4,`R1   };
+	11: oInstruction = { `NOP, 24'd4000  };
+	12: oInstruction = { `STO, `R4, 16'd242 };
+	13: oInstruction = { `STO, `R1, 16'd242 };
+	14: oInstruction = { `NOP, 24'd4000  };
+	15: oInstruction = { `STO, `R4, 16'd243 };
+	16: oInstruction = { `STO, `R1, 16'd243 };
+	17: oInstruction = { `VGA, `R4,`R1   };
+	18: oInstruction = { `NOP, 24'd4000  };
+	19: oInstruction = { `STO, `R4, 16'd244 };
+	20: oInstruction = { `STO, `R1, 16'd244 };
+	21: oInstruction = { `VGA, `R4,`R1   };		
+*/	22: oInstruction = { `NOP, 24'd4000  };
+	23: oInstruction = { `JMP, 8'd0,16'b0};
 	default:
 		oInstruction = { `LED ,  24'b10101010 };		//NOP
 	endcase
