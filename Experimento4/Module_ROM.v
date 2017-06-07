9b23c04468978157338c5389258c4eeca61be525
moi6ba <moi6ba@hotmail.com>
2017-06-07 10:01:02 -0600

    Update Module_ROM.v

--- Experimento4/Module_ROM.v
+++ Experimento4/Module_ROM.v
@@ -3,6 +3,7 @@
 
 `define LOOP1 8'd8
 `define LOOP2 8'd5
+
 module ROM
 (
 	input  wire[15:0]  		iAddress,
@@ -14,26 +15,25 @@ begin
 
 //LOOP1:
 	0: oInstruction  = { `NOP, 24'd4000     };
-	1: oInstruction  = { `STO, `R4, 16'd240 };
-	2: oInstruction  = { `STO, `R1, 16'd240 };
-	6: oInstruction  = { `VGA, `COLOR_GREEN, `R4,`R1   };
-	7: oInstruction  = { `NOP, 24'd4000  	};
-	8: oInstruction  = { `STO, `R4, 16'd241 };
-	9: oInstruction  = { `STO, `R1, 16'd241 };
-	10: oInstruction = { `VGA, `COLOR_GREEN, `R4,`R1   };
-	11: oInstruction = { `NOP, 24'd4000  	};
-	12: oInstruction = { `STO, `R4, 16'd242 };
-	13: oInstruction = { `STO, `R1, 16'd242 };
-	14: oInstruction = { `NOP, 24'd4000  	};
-	15: oInstruction = { `STO, `R4, 16'd243 };
-	16: oInstruction = { `STO, `R1, 16'd243 };
-	17: oInstruction = { `VGA, `COLOR_GREEN, `R4,`R1   };
-	18: oInstruction = { `NOP, 24'd4000  	};
-	19: oInstruction = { `STO, `R4, 16'd244 };
-	20: oInstruction = { `STO, `R1, 16'd244 };
-	21: oInstruction = { `VGA, `COLOR_GREEN, `R4,`R1   };		
-	22: oInstruction = { `NOP, 24'd4000  	};
-	23: oInstruction = { `JMP, 8'd0,16'b0	};
+        
+	1: oInstruction  = { `STO, `R0, 16'd250 };
+	2: oInstruction  = { `STO, `R1, 16'd250 }; \\ Define las posiciones maximas
+	3: oInstruction  = { `STO, `R2, 1 };
+	
+	4: oInstruction  = { `STO, `R3, 16'd240 };	
+	5: oInstruction  = { `STO, `R4, 16'd240 }; \\Posiciones iniciales
+		
+	6: oInstruction = { `NOP, 24'd4000  	};
+	7: oInstruction  = { `VGA, `COLOR_GREEN, `R3,`R4}; \\Color Verde en posicion actual
+	8: oInstruction = { `NOP, 24'd4000  	};
+		
+	9: oInstruction  = { `ADD, `R4, 1 };
+	10: oInstruction  = { `BLE, 16'd6, `R4, `R0}; \\ Salta si Columna menor a valor maximo
+	11: oInstruction  = { `ADD, `R3, 1 };
+	12: oInstruction  = { `BLE, 16'd5, `R4, `R0}; \\ Salta si fila menor a valor maximo	
+		
+
+	13: oInstruction = { `JMP, 8'd0,16'b0	};
 	default:
 		oInstruction = { `LED ,  24'b10101010 };		//NOP
 	endcase
